c75ceb0036468e80c60cb62edbe14c3e8257edbc
william-oliveira-da-silva <wyll.hello@gmail.com>
2020-02-21 00:18:32 -0400

    20/02/20

---
+++ aula01/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder= "Insira o valor aqui" 
+      style={styles.input}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    padding: 16,
+    bordeeWidth: 1,
+    borderColor: '#f00',
+    borderRadius: 5,
+  },
+});
